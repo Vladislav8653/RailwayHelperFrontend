388d2dfb1c325f06813e3a095af5cb878ee436c3
Владислав Арефин <arefin@topcase.ru>
2026-03-19 15:47:43 +0100

    Create API call for place info

--- src/js/api/stationApi.js
+++ src/js/api/stationApi.js
@@ -1,14 +1,27 @@
 const BASE_URL = "http://localhost:5196";
 
 export async function fetchStations(query) {
-    if (!query || query.length < 3) return [];
-
+    if (!query) return [];
+    
     const url = `${BASE_URL}/search?name=${encodeURIComponent(query)}`;
     const response = await fetch(url);
 
     if (!response.ok) {
-        throw new Error(`Ошибка API: ${response.status}`);
+        throw new Error(`Ошибка при получении станций: ${response.status}`);
     }
 
     return await response.json(); 
+}
+
+export async function fetchPlaceInfo(params) {
+    const queryString = new URLSearchParams(params).toString();
+    const url = `${BASE_URL}/place?${queryString}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Ошибка при проверке места: ${response.status}`);
+    }
+
+    return await response.json();
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
 import { ScreenManager } from './ui/ScreenManager.js';
 import {SearchForm} from "./ui/SearchForm.js";
+import {fetchPlaceInfo, fetchStations} from "./api/stationApi.js";
 
 const screens = new ScreenManager();
 
 document.getElementById("start-btn").onclick = () => screens.show("search");
 document.getElementById("back-btn").onclick = () => screens.show("welcome");
 
-const searchForm = new SearchForm("main-form", async (dto) => {
-    console.log("Отправка DTO на сервер:", dto);
-    // Здесь будет fetch(url, { body: JSON.stringify(dto) ... })
-    await new Promise(r => setTimeout(r, 1400));
+const searchForm = new SearchForm("main-form", async (params) => {
+    console.log("Отправка DTO на сервер:", params);
+    const result = await fetchPlaceInfo(params);
+
+    console.log("Результат от сервера:", result);
+
+    return result;
 });
